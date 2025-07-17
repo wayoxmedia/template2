d01969035bec00fe399435ee9560f06f726b36d1
Eduardo Sanchez <walkovik@gmail.com>
2025-07-17 11:59:43 -0400

    Added assets folder

--- html/scripts.php
+++ html/scripts.php
@@ -5,4 +5,4 @@
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
         crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>
-<script src="assets/js/script.min.js"></script>
+<script src="assets/js/script.js"></script>
