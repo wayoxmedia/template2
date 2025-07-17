9da6848110475b1171c4997aa451cff816ac240b
Eduardo Sanchez <walkovik@gmail.com>
2025-07-17 12:33:03 -0400

    Updated footer

--- html/footer.php
+++ html/footer.php
@@ -4,12 +4,12 @@
       <div class="col-md-3 col-sm-6">
         <div class="footer-menu footer-menu-001">
           <div class="footer-intro mb-4">
-            <a href="index.html">
+            <a href="#">
               <img src="images/main-logo.png" alt="logo">
             </a>
           </div>
-          <p>Gravida massa volutpat aenean odio. Amet, turpis erat nullam fringilla elementum diam in. Nisi, purus
-            vitae, ultrices nunc. Sit ac sit suscipit hendrerit.</p>
+          <p>Gravida massa volutpat aenean odio. Amet, turpis erat nullam fringilla elementum diam in.
+            Nisi, purus vitae, ultrices nunc. Sit ac sit suscipit hendrerit.</p>
           <div class="social-links">
             <ul class="list-unstyled d-flex flex-wrap gap-3">
               <li>
@@ -56,16 +56,16 @@
           <h5 class="widget-title text-uppercase mb-4">Quick Links</h5>
           <ul class="menu-list list-unstyled text-uppercase border-animation-left fs-6">
             <li class="menu-item">
-              <a href="index.html" class="item-anchor">Home</a>
+              <a href="#" class="item-anchor">Home</a>
             </li>
             <li class="menu-item">
-              <a href="index.html" class="item-anchor">About</a>
+              <a href="#" class="item-anchor">About</a>
             </li>
             <li class="menu-item">
-              <a href="blog.html" class="item-anchor">Services</a>
+              <a href="#" class="item-anchor">Services</a>
             </li>
             <li class="menu-item">
-              <a href="styles.html" class="item-anchor">Single item</a>
+              <a href="#" class="item-anchor">Single item</a>
             </li>
             <li class="menu-item">
               <a href="#" class="item-anchor">Contact</a>
@@ -93,7 +93,7 @@
               <a href="#" class="item-anchor">Find us easy</a>
             </li>
             <li class="menu-item">
-              <a href="index.html" class="item-anchor">Faqs</a>
+              <a href="#" class="item-anchor">Faqs</a>
             </li>
           </ul>
         </div>
@@ -101,10 +101,12 @@
       <div class="col-md-3 col-sm-6">
         <div class="footer-menu footer-menu-004 border-animation-left">
           <h5 class="widget-title text-uppercase mb-4">Contact Us</h5>
-          <p>Do you have any questions or suggestions? <a href="mailto:contact@yourcompany.com"
-                                                          class="item-anchor">contact@yourcompany.com</a></p>
-          <p>Do you need support? Give us a call. <a href="tel:+43 720 11 52 78" class="item-anchor">+43 720 11 52
-              78</a>
+          <p>Do you have any questions or suggestions?
+            <a href="mailto:contact@yourcompany.com"
+               class="item-anchor">contact@yourcompany.com</a>
+          </p>
+          <p>Do you need support? Give us a call.
+            <a href="tel:+43 720 11 52 78" class="item-anchor">+43 720 11 52 78</a>
           </p>
         </div>
       </div>
@@ -127,9 +129,12 @@
           </div>
         </div>
         <div class="col-md-6 text-end">
-          <p>© Copyright 2022 Kaira. All rights reserved. Design by <a href="https://templatesjungle.com"
-                                                                       target="_blank">TemplatesJungle</a> Distribution By <a href="https://themewagon.com"
-                                                                                                                              target="blank">ThemeWagon</a></p>
+          <p>© Copyright <?= date('Y') .' ' . SITE_NAME?>. All rights reserved.
+            Design by
+            <a href="https://wayoxmedia.com"
+               target="_blank">Wayox Media
+            </a>
+          </p>
         </div>
       </div>
     </div>
